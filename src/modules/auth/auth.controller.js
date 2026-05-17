70a730d62d3502c5fc79428f31c5216c3086561c
Omixo <alluomi55@gmail.com>
2026-05-18 00:32:30 +0530

    added the controller for the Auth

---
+++ src/modules/auth/auth.controller.js
@@ -0,0 +1,55 @@
+const { registerUser, loginUser, logoutUser, changePassword } = require('./auth.service')
+const { sendSuccess, sendError } = require('../../utils/response.utils')
+///  for the register user also status code 201 for new user 
+const register = async (request, reply) => {
+  try {
+    const user = await registerUser(request.body)
+    return sendSuccess(reply, user, 'User registered successfully', 201)
+  } catch (err) {
+    return sendError(reply, err.message, 400)
+  }
+}
+
+// for the login user also status code 200 for successful login
+
+const login = async (request, reply) => {
+  try {
+    const data = await loginUser(request.body)
+    return sendSuccess(reply, data, 'Login successful')
+  } catch (err) {
+    return sendError(reply, err.message, 401)
+  }
+}
+
+// for the logout user also status code 200 for successful logout
+
+const logout = async (request, reply) => {
+  try {
+    await logoutUser({
+      userId: request.user.id,
+      refreshToken: request.body.refreshToken
+    })
+    return sendSuccess(reply, null, 'Logged out successfully')
+  } catch (err) {
+    return sendError(reply, err.message, 400)
+  }
+}
+
+
+// for the change password also status code 200 for successful password change
+
+const changeUserPassword = async (request, reply) => {
+  try {
+    await changePassword({
+      userId: request.user.id,
+      ...request.body
+    })
+    return sendSuccess(reply, null, 'Password changed successfully')
+  } catch (err) {
+    return sendError(reply, err.message, 400)
+  }
+}
+
+
+// exports ()
+module.exports = { register, login, logout, changeUserPassword }
